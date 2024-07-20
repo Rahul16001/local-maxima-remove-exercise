e88cb90bf6a4213df928b134f05b79aa916096f5
Rahul D <rahul_d@epam.com>
2024-07-21 01:17:37 +0530

    My first commit

--- src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
+++ src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
@@ -1,6 +1,8 @@
 package com.epam.rd.autotasks.arrays;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class LocalMaximaRemove {
 
@@ -10,10 +12,45 @@ public class LocalMaximaRemove {
         System.out.println(Arrays.toString(removeLocalMaxima(array)));
     }
 
-    public static int[] removeLocalMaxima(int[] array){
+    public static int[] removeLocalMaxima(int[] arr){
 
         //put your code here
+        ArrayList<Integer> list = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+        boolean flag;
+        for(int i=0; i<arr.length;i++)
+        {
+            int left = i - 1;
+            int right = i + 1;
 
-        throw new UnsupportedOperationException();
+            flag = true;
+            if(left >= 0)
+            {
+                if(arr[i] <= arr[left]) flag = false;
+            }
+            if(right < arr.length)
+            {
+                if(arr[i] <= arr[right]) flag = false;
+            }
+            if(flag) set.add(i);// if flag is true then it's local maxima add it's index to set
+        }
+
+        for(int i=0;i<arr.length;i++)
+        {
+            if(!set.contains(i))
+            {
+                list.add(arr[i]);
+            }
+        }
+
+        int []ans = new int[list.size()];
+        int counter = 0;
+        for(int x : list)
+        {
+            ans[counter] = x;
+            counter++;
+        }
+
+        return ans;
     }
 }
